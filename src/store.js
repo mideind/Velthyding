05523ad4a2f178fc130729343b2ff02386ca09d4
Haukur Páll <haukurpalljonsson@gmail.com>
2021-07-06 15:16:56 +0000

    Fixing linting issues

--- src/store.js
+++ src/store.js
@@ -1,9 +1,7 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-
-import logger from "redux-logger";
-
 import { loginReducer } from "features/login/loginSlice";
 import { translateReducer } from "features/translate/translateSlice";
+import logger from "redux-logger";
 
 const reducer = {
   login: loginReducer,
@@ -12,6 +10,7 @@ const reducer = {
 
 const middleware = [...getDefaultMiddleware(), logger];
 
+// eslint-disable-next-line import/prefer-default-export
 export const store = configureStore({
   reducer,
   middleware,
