813b815b7f3fc2eb453232f9844d569e690b78f4
Haukur Páll <haukurpalljonsson@gmail.com>
2021-07-29 11:40:35 +0000

    Removing docker packing and replacing it with zip packaging

--- build_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Use this script to build a docker image for velthyding
-PACKAGE_VERSION=$(grep -m1 version package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
-docker build -t docker.greynir.is/velthyding:$PACKAGE_VERSION .
--- build_web_frontend.sh
+++ build_web_frontend.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# Use this script to build the project without installing node.
+# Use this script to build the frontend without installing node.
 docker run -ti \
   --rm \
   -e NODE_ENV=production \
   -v $PWD:/app \
   --user $(id -u):$(id -g) \
   node:14-alpine sh -c "cd /app; npm run-script build"
+# Get the version number from package.json
+PACKAGE_VERSION=$(grep -m1 version package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
+# Then we package the build directory into a zip file.
+zip -r velthyding-v$PACKAGE_VERSION.zip build
