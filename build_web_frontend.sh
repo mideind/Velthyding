0d52b0c6d387f55c582b168d50275b3bcb498702
Haukur Páll <haukurpalljonsson@gmail.com>
2021-07-29 14:04:31 +0000

    No zip file - but plain rsync

--- build_web_frontend.sh
+++ build_web_frontend.sh
@@ -8,5 +8,6 @@ docker run -ti \
   node:14-alpine sh -c "cd /app; npm run-script build"
 # Get the version number from package.json
 PACKAGE_VERSION=$(grep -m1 version package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
-# Then we package the build directory into a zip file.
-zip -r velthyding-v$PACKAGE_VERSION.zip build
+echo "Successfully built $PACKAGE_VERSION"
+echo "You can now upload it to maja with the following command:"
+echo "rsync -Phrahz build/* maja:/data2/velthyding_releases/v$PACKAGE_VERSION"
