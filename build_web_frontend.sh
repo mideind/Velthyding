6daaa5a90eb79d387f270e9506252851d78b810c
Haukur Páll <haukurpalljonsson@gmail.com>
2021-07-29 14:28:41 +0000

    Review fixes - more consise rsync command

--- build_web_frontend.sh
+++ build_web_frontend.sh
@@ -10,4 +10,4 @@ docker run -ti \
 PACKAGE_VERSION=$(grep -m1 version package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
 echo "Successfully built $PACKAGE_VERSION"
 echo "You can now upload it to maja with the following command:"
-echo "rsync -Phrahz build/* maja:/data2/velthyding_releases/v$PACKAGE_VERSION"
+echo "rsync -Phazv build/* maja:/data2/velthyding_releases/v$PACKAGE_VERSION"
