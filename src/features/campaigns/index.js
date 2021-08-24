d33e55ee70a317f2311100ac8cd5122903a09441
Svanhvít <svanhviti16@ru.is>
2021-08-24 10:52:54 +0000

    formatting

--- src/features/campaigns/index.js
+++ src/features/campaigns/index.js
@@ -8,7 +8,7 @@ import {
   Grid,
   Modal,
   Progress,
-  Table
+  Table,
 } from "semantic-ui-react";
 
 function TaskCard(props) {
@@ -156,7 +156,11 @@ function CampaignTableCell(props) {
       </Table.Cell>
       <Table.Cell>{props.ends}</Table.Cell>
       <Table.Cell>
-        <Progress total={total} value={value} progress="percent" precision="0" success />
+        <Progress total={total}
+                  value={value}
+                  progress="percent"
+                  precision="0"
+                  success />
       </Table.Cell>
     </>
   );
