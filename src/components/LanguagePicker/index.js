250269c5ab12b3440b54889adc4d903654bfdff0
Haukur Páll <haukurpalljonsson@gmail.com>
2022-06-13 15:05:56 +0000

    Adding Polish and Faroese to the language dropdown.

--- src/components/LanguagePicker/index.js
+++ src/components/LanguagePicker/index.js
@@ -1,29 +1,15 @@
-import { switchLanguage } from "features/translate/translateSlice";
-import React from "react";
+import {
+  setSourceLanguage,
+  setTargetLanguage,
+  swapLanguages,
+} from "features/translate/translateSlice";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import { Button, Dropdown } from "semantic-ui-react";
 import "./index.css";
 
 function LanguagePicker(props) {
-  const { t } = useTranslation();
-  const LANGUAGE_OPTIONS = [
-    {
-      key: "is",
-      text: t("Icelandic"),
-      value: "is",
-      flag: "is",
-    },
-    {
-      key: "en",
-      text: t("English"),
-      value: "en",
-      flag: "uk",
-    },
-  ];
-
-  const { sourceLang, targetLang } = useSelector((state) => state.translation);
-  const dispatch = useDispatch();
   return (
     <Dropdown
       button
@@ -31,32 +17,88 @@ function LanguagePicker(props) {
       floating
       labeled
       icon="world"
-      options={LANGUAGE_OPTIONS}
+      options={props.languageOptions}
       selection
-      value={!props.isSource ? targetLang : sourceLang}
-      onChange={() => dispatch(switchLanguage())}
+      value={props.language}
+      onChange={props.changeLanguage}
       placeholder={props.text}
     />
   );
 }
 
 const LanguageDirectionPicker = () => {
+  const { t } = useTranslation();
+  const { sourceLang, targetLang } = useSelector((state) => state.translation);
+  const [tgtLanguageOptions, setTgtLanuageOptions] = useState([]);
   const dispatch = useDispatch();
-
+  // TODO: Get this information from the backend
+  const SUPPORTED_LANGUAGE_PAIRS = {
+    en: ["is", "fo"],
+    is: ["en", "pl"],
+    pl: ["is"],
+    fo: ["en"],
+  };
+  const LANGUAGE_OPTIONS = {
+    is: {
+      key: "is",
+      text: t("Icelandic"),
+      value: "is",
+      flag: "is",
+    },
+    en: {
+      key: "en",
+      text: t("English"),
+      value: "en",
+      flag: "uk",
+    },
+    pl: {
+      key: "pl",
+      text: t("Polish"),
+      value: "pl",
+      flag: "pl",
+    },
+    fo: {
+      key: "fo",
+      text: t("Faroese"),
+      value: "fo",
+      flag: "fo",
+    },
+  };
+  useEffect(() => {
+    // TODO: Consider moving valid target lang logic to redux
+    const validTgtLanguages = SUPPORTED_LANGUAGE_PAIRS[sourceLang];
+    setTgtLanuageOptions(validTgtLanguages.map((key) => LANGUAGE_OPTIONS[key]));
+    // If the currently selected targetLang is not in our dropdown list select the first as targetLang
+    if (validTgtLanguages.indexOf(targetLang) === -1) {
+      const newTargetLang = validTgtLanguages[0];
+      dispatch(setTargetLanguage(newTargetLang));
+    }
+  }, [sourceLang]);
+  const SRC_LANGUAGE_OPTIONS = Object.keys(LANGUAGE_OPTIONS).map(
+    (key) => LANGUAGE_OPTIONS[key]
+  );
   return (
     <div className="LanguageDirectionPicker">
       <div className="LanguageDirection">
-        <LanguagePicker isSource />
+        <LanguagePicker
+          languageOptions={SRC_LANGUAGE_OPTIONS}
+          language={sourceLang}
+          changeLanguage={(_, data) => {
+            dispatch(setSourceLanguage(data.value));
+          }}
+        />
         <span className="LanguageToggle-button">
-          <Button
-            icon="exchange"
-            // We no longer switch the text
-            onClick={() => dispatch(switchLanguage())}
-          />
+          <Button icon="exchange" onClick={() => dispatch(swapLanguages())} />
         </span>
       </div>
       <div className="LanguageDirection">
-        <LanguagePicker />
+        <LanguagePicker
+          languageOptions={tgtLanguageOptions}
+          language={targetLang}
+          changeLanguage={(_, data) => {
+            dispatch(setTargetLanguage(data.value));
+          }}
+        />
       </div>
     </div>
   );
